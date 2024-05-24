4cb50e9ce6d01d2ffbda2a11041bf542f91e0d21
MostlyK <135974627+MostlyKIGuess@users.noreply.github.com>
2024-05-24 23:06:16 +0530

    huh favicon

--- src/app/components/misccard.tsx
+++ src/app/components/misccard.tsx
@@ -39,10 +39,5 @@ const testimonials = [
     name: "Jane Austen",
     title: "Pride and Prejudice",
   },
-  {
-    quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
-  },
+
 ];
