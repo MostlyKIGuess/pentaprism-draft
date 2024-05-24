640d18d5da8656a35e6f7adccf41b1bf05c98e05
MostlyK <135974627+MostlyKIGuess@users.noreply.github.com>
2024-05-24 23:03:20 +0530

    added favicon and metadata

--- src/app/components/misccard.tsx
+++ src/app/components/misccard.tsx
@@ -18,20 +18,20 @@ export function Misccard() {
 const testimonials = [
   {
     quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Charles Dickens",
-    title: "A Tale of Two Cities",
+      "Don't force yourself to take pictures when you travel somewhere. Let them flow to you. Look at the scenery, absorb it. Try to imagine the possible shots, and which type will capture the essence you wish to portray. Be creative. Use the surroundings to get something unique. Sometimes, edits are necessary to bring life to your click. Lastly, always remember. Learning photography is an everlasting process. There is always something to learn.",
+    name: "Soumil",
+    title: "Let it flow",
   },
   {
     quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "William Shakespeare",
-    title: "Hamlet",
+      "Enough talks send pics now ;)",
+      name: "Soumil",
+    title: "A beautiful whatsapp",
   },
   {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
+    quote: "Capture the love in photos",
+    name: "Roshan",
+    title: "Passion",
   },
   {
     quote:
