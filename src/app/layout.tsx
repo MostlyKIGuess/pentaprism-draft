827bb65fab3b07925a3b6bdc0b51b938bb29bbcd
MostlyK <135974627+MostlyKIGuess@users.noreply.github.com>
2024-05-24 23:18:09 +0530

    man i am dumb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
       <link rel="icon" href="/favicon.ico" sizes="any"/>
-
+      
+      </head>
       <body className={inter.className}>
         {/* <NavbarDemo /> */}
         {children}</body>
